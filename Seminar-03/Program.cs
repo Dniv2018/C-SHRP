469de26fe52e770d455880c0d08628053da0b4a9
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-11-06 21:54:39 +0300

    Homework is done

--- Seminar-03/Program.cs
+++ Seminar-03/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine();
 Console.WriteLine("Урок 3. Массивы и функции в программировании");
 Console.WriteLine();
 Console.WriteLine("Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Палиндром - это слово, число, фраза или другая последовательность символов, которые читаются одинаково как в обратном, так и в прямом направлении, например, слова \"мадам\" или 12321.");
-int inputNumber;
+int inputNumber = 0;
 int digit;
 int[] array = new int[5];
 bool checkPalindrom = true; ;
@@ -20,7 +20,7 @@ while (true)
     do
     {
         Console.Write("Enter a palindrome: ");
-        getNumber("Enter a palindrome: ");
+        inputNumber = getNumber("This is not number. Enter a palindrome: ", inputNumber);
         digit = CountDigits(inputNumber);
         if (digit != 5)
         {
@@ -46,7 +46,7 @@ while (true)
     // output the check result
     if (checkPalindrom)
     {
-        Console.WriteLine("This IS palindrome.");
+        Console.WriteLine("This IS palindrome: ");
         break;
     }
     else
@@ -59,20 +59,103 @@ while (true)
 
 
 // Задача 21
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Напишите программу, которая принимает на вход координаты двух точек (A и B) и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine();
+Console.WriteLine("Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+int[,] points = new int[2, 3];
+
+Console.WriteLine("Enter coordinates of the A point");
+Console.Write("Enter the X: ");
+points[0, 0] = getNumber("This is not number. Enter the X: ", points[0, 0]);
+Console.Write("Enter the Y: ");
+points[0, 1] = getNumber("This is not number. Enter the Y: ", points[0, 1]);
+Console.Write("Enter the Z: ");
+points[0, 2] = getNumber("This is not number. Enter the Z: ", points[0, 2]);
+
+Console.WriteLine("Enter coordinates of the B point");
+Console.Write("Enter the X: ");
+points[1, 0] = getNumber("This is not number. Enter the X: ", points[1, 0]);
+Console.Write("Enter the Y: ");
+points[1, 1] = getNumber("This is not number. Enter the Y: ", points[1, 1]);
+Console.Write("Enter the Z: ");
+points[1, 2] = getNumber("This is not number. Enter the Z: ", points[1, 2]);
+
+double res = 0;
+for (int i = 0; i < 3; i++)
+{
+    res += Math.Pow(points[0, i] - points[1, i], 2);
+}
+Console.WriteLine("Distance between points A and B: " + Math.Sqrt(res));
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine();
+Console.WriteLine("Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+
+Console.Write("Enter number: ");
+inputNumber = getNumber("This is not number. Enter the NUMBER: ", inputNumber);
+
+Console.Write(inputNumber + " -> ");
+int sign = 1;
+if (inputNumber < 0) sign = -1;
+for (int i = 1; i <= Math.Abs(inputNumber); i++)
+{
+    Console.Write(Math.Pow(i, 3) * sign);
+    if (i < Math.Abs(inputNumber)) Console.Write(", ");
+}
+Console.WriteLine();
+
+// Дополнительно.
+// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
+// 11 -> 2, 3, 5 , 7, 11
+Console.WriteLine();
+Console.WriteLine("Дополнительно. Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.");
+
+Console.Write("Enter number: ");
+inputNumber = getNumber("This is not number. Enter the NUMBER: ", inputNumber);
+
+bool check;
+Console.Write(inputNumber + " -> ");
+for (int i = 1; i <= inputNumber; i++)
+{
+    check = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            check = false;
+            break;
+        }
+    }
+
+    if (check)
+    {
+        Console.Write(i);
+        if (i < inputNumber) Console.Write(", ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine();
+
+
 
 
 
 // Methods:
 //
-void getNumber(string message)
+int getNumber(string message, int Number)
 {
-    while (!int.TryParse(Console.ReadLine(), out inputNumber))
+    while (!int.TryParse(Console.ReadLine(), out Number))
     {
-        Console.Write("This is not number. " + message);
+        Console.Write(message);
     }
+    return Number;
 }
 
 int CountDigits(int number)
@@ -108,13 +191,3 @@ int GetSeparator(int repeat)
     }
     return div;
 }
-
-
-// Задача 23
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-// Дополнительно.
-// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
-// 11 -> 2, 3, 5 , 7, 11
