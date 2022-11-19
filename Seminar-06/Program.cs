cfcfafa44c1781ebdb2c8ea96cf787e95b8b3b6a
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-11-19 17:49:24 +0300

    Task 41 done

--- Seminar-06/Program.cs
+++ Seminar-06/Program.cs
@@ -18,18 +18,63 @@ Console.WriteLine("[ " + string.Join(", ", len) + " ] -> " + CheckTriangle(len))
 Console.WriteLine();
 Console.WriteLine("Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.");
 string result = "";
-Console.WriteLine("Decimal: 45 -> " + GetBynaryFromDecimal(45, result));
-string GetBynaryFromDecimal(int dec, string res)
+int DEC = 46;
+// GetBynaryFromDecimal(DEC);
+Console.WriteLine($"Decimal: {DEC} -> " + GetBynaryFromDecimal(DEC));
+
+// Урок 6. Двумерные массивы и рекурсия
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine();
+Console.WriteLine("Урок 6. Двумерные массивы и рекурсия.");
+Console.WriteLine();
+Console.WriteLine("Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+// Console.WriteLine("Enter the numbers in one line separated by a space.");
+string str;
+do
 {
-    if (dec / 2 > 0) return GetBynaryFromDecimal(dec / 2, res);
-    else return dec % 2 + res;
-    // if (dec / 2 == 0) return dec % 2 + res;
-    // else GetBynaryFromDecimal(dec / 2, res);
-    // return res;
+    Console.WriteLine("Enter the numbers in one line separated by a space.");
+    str = Console.ReadLine()!;
+} while (String.IsNullOrWhiteSpace(str));
+// Console.WriteLine("Enter the numbers in one line separated by a space.");
+// string str = Console.ReadLine()!;
+str = str.Trim();
+string[] subs = str.Split(' ');
+int[] num = new int[str.Length];
+
+for (int i = 0; i < subs.Length; i++)
+{
+    if (!int.TryParse(subs[i], out num[i]))
+    {
+        num[i] = 0;
+    }
 }
+int count = 0;
+
+foreach (int item in num)
+{
+    if (item > 0) count++;
+}
+Console.WriteLine($"You entered {count} numbers greater than 0");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Дополнительно. Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент.
+
 
 
 // Methods:
+string GetBynaryFromDecimal(int dec)
+{
+    result = dec % 2 + result;
+    if (dec / 2 == 0) return result;
+    else GetBynaryFromDecimal(dec / 2);
+    return result;
+}
+
 bool CheckTriangle(int[] array)
 {
     bool count;
